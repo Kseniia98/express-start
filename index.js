9a3e6ce92f7ced82d96ffd34629b66b0beb22eb0
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-06-03 16:24:14 +0300

    Add nodemon

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const http = require('http');
+const express = require('express');
 
-// Create a local server to receive data from
-const server = http.createServer((req, res) => {
-  console.log(req);
+const app = express();
 
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!'
-  }));
+app.get('/', (req, res) => {
+  console.log('hi');
 
-  if(req.method == 'POST' && req.url.includes === '/workspaces'){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('OK');
-    return;
-  }
-
-  if(req.method == 'GET' && req.url.includes === '/workspaces'){
-    const id = req.url.split('/')[1];
-    
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('DELETED' + id);
-    return;
-  }
+  res.end();
 });
 
-server.listen(5001);
-//http://localhost:6000
+app.listen(5001, () => {
+  console.log('Server started');
+});
